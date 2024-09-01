6b66e1f75abea6c53a11bff16b3925a9fccdbe1b
mariosupreme250 <l211080399@iztapalapa.tecnm.mx>
2024-08-31 19:11:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-eyy
+const btnCart = document.querySelector('.container-icon')
+const containerCartProducts = document.querySelector('container-cart-products')
+btnCart.addEventListener('click', () => {
+  containerCartProducts.classlist.toggle('hidden-cart')
+}
